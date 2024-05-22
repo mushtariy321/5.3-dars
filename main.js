cea168075505bcb56c9b7e3579913563720b3705
mushtariy321 <mushtariyrixsiboyev5a@gmail.com>
2024-05-22 16:31:03 +0300

    homework done

---
+++ main.js
@@ -0,0 +1,22 @@
+// let obj = {
+//     Id: 5,
+//     IsStudent: true,
+//     userName: `Dilbek`,
+//     password: 7474,
+// }
+// for (let key in obj) {
+//     console.log(key + 'A' + `:` + obj[key]);
+// }
+// Object.freeze(obj)
+// obj.Id = 12
+// console.log(obj);
+
+
+let obj = {
+    name: "John",
+    Age: 30,
+    City: "New York",
+}
+for (let key in obj) {
+    console.log(Xobj[key]);
+}
